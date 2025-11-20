852b51dca28b56f6e4c07e8f95a7753b3d6a64c6
necaran <55765083+necaran@users.noreply.github.com>
2025-11-20 18:48:36 +0800

    linux: interface filtering by regex

--- scripts/network-bandwidth.sh
+++ scripts/network-bandwidth.sh
@@ -21,11 +21,8 @@ get_bandwidth_for_osx() {
 }
 
 get_bandwidth_for_linux() {
-  netstat -ie | awk '
-    match($0, /RX([[:space:]]packets[[:space:]][[:digit:]]+)?[[:space:]]+bytes[:[:space:]]([[:digit:]]+)/, rx) { rx_sum+=rx[2]; }
-    match($0, /TX([[:space:]]packets[[:space:]][[:digit:]]+)?[[:space:]]+bytes[:[:space:]]([[:digit:]]+)/, tx) { tx_sum+=tx[2]; }
-    END { print rx_sum, tx_sum }
-  '
+  local x=$(get_tmux_option "@tmux-network-interface-regex" '^(eth|en|wl)')
+  awk -v x="$x" 'NR>2 {if($1~x){r+=$2;t+=$10}} END {print r,t}' /proc/net/dev
 }
 
 get_bandwidth() {
@@ -67,7 +64,7 @@ main() {
     local second_measure=( $(get_bandwidth $os) )
     local download_speed=$(((${second_measure[0]} - ${first_measure[0]}) / $sleep_time))
     local upload_speed=$(((${second_measure[1]} - ${first_measure[1]}) / $sleep_time))
-    $(set_tmux_option "@network-bandwidth-previous-value" "↓$(format_speed $download_speed) • ↑$(format_speed $upload_speed)")
+    $(set_tmux_option "@network-bandwidth-previous-value" "↓$(format_speed $download_speed) ↑$(format_speed $upload_speed)")
   fi
 
   echo -n "$(get_tmux_option "@network-bandwidth-previous-value")"
